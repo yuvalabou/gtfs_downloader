2dc9eb661ee1e1d0e1c977e62731ae7120b79052
Yuval Aboulafia <yuval.abou@gmail.com>
2020-06-23 22:20:04 +0300

    ugly update

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 Python app to retrive and extract gtfs data for israely public transport
+
+#### Requierments
+
+- tqdm
+
+### features:
+app compares the local against the remote file for up to date data
+
+### How to use:
--- gtfs_updater.py
+++ gtfs_updater.py
@@ -10,26 +10,26 @@ savedir = "D:/Temp/"
 gtfs_dir = "D:/gtfs/"
 gtfs = "israel-public-transportation.zip"
 
-if not os.path.isdir(savedir):
-    print('The directory is not present. Creating a new one..')
-    os.mkdir(savedir)
-else:
-    print('The directory is present.')
-
-os.chdir(savedir)
-
 ftp = FTP('199.203.58.18')
 ftp.login()
 print("Connected")
+
+if not os.path.isdir(savedir):
+    print('Temp directory is not present. Creating a new one.')
+    os.mkdir(savedir)
+else:
+    print('Temp directory is present.')
+
+os.chdir(savedir)
 
 remote_datetime = ftp.voidcmd("MDTM " + gtfs)[4:].strip()
 remote_timestamp = time.mktime(time.strptime(remote_datetime, '%Y%m%d%H%M%S'))
 
 if os.path.isfile(savedir + "/" + gtfs):
-    print("Found local file")
+    print("Found local gtfs archive.")
 
 else:
-    print("Local file not found, Downloading.")
+    print("Local gtfs archive not found, Downloading.")
     with open(gtfs, 'wb') as fd:
         total = int(ftp.size(gtfs))
 
@@ -44,28 +44,35 @@ else:
 local_gtfs_timestamp = os.path.getmtime(gtfs)
 
 if int(remote_timestamp) > int(local_gtfs_timestamp):
-    print("Retriving up to date file")
-    file = open(gtfs, "wb")
-    ftp.retrbinary("RETR " + gtfs, file.write)
-    file.close()
-    print("Retrived up to date file")
+    print("Local file has expired, Retriving up to date file")
+    with open(gtfs, 'wb') as fd:
+        total = int(ftp.size(gtfs))
+
+        with tqdm(total=total, unit='B', unit_scale=True) as pbar:
+            def cb(data):
+                l = int(len(data))
+                pbar.update(l)
+                fd.write(data)
+
+            ftp.retrbinary('RETR {}'.format(gtfs), cb)
+
 else:
     print("Local file is up to date.")
 
 FTP.quit(ftp)
 
 if not os.path.isdir(gtfs_dir):
-    print('The directory is not present. Creating a new one..')
+    print('Working directory is not present. Creating a new one.')
     os.mkdir(gtfs_dir)
 else:
-    print('The directory is present.')
+    print('Working directory is present.')
 
 with zipfile.ZipFile(gtfs) as zip:
     print("Extracting")
-    for member in tqdm(zip.infolist(), total=len(zip.namelist()), unit='Files'):
+    for item in tqdm(zip.infolist(), total=len(zip.namelist()), unit='Files'):
         zip.filename = os.path.basename(zip.filename)
         try:
-            zip.extract(member, gtfs_dir)
+            zip.extract(item, gtfs_dir)
         except zipfile.error() as error:
             pass
 
