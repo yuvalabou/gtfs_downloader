8a32a37b663037d9427eeab224e39eb39b02e6b4
Yuval Aboulafia <yuval.abou@gmail.com>
2020-08-15 22:36:16 +0300

    Cleanup

--- gtfs_updater.py
+++ gtfs_updater.py
@@ -1,22 +1,20 @@
 from ftplib import FTP
 import zipfile
 import os
-import glob
-import shutil
 import time
 from tqdm import tqdm
 
-savedir = "D:/Temp/"
-gtfs_dir = "D:/gtfs/"
-gtfs = "israel-public-transportation.zip"
+SAVE_DIR = "D:/Temp/"
+GTFS_DIR = "D:/gtfs/"
+GTFS_NAME = "israel-public-transportation.zip"
 
 ftp = FTP('199.203.58.18')
 ftp.login()
 print("Connected")
 
 def gtfs_download():
-    with open(gtfs, 'wb') as fd:
-        total = int(ftp.size(gtfs))
+    with open(GTFS_NAME, 'wb') as fd:
+        total = int(ftp.size(GTFS_NAME))
 
         with tqdm(total=total, unit='B', unit_scale=True) as pbar:
             def cb(data):
@@ -24,29 +22,29 @@ def gtfs_download():
                 pbar.update(l)
                 fd.write(data)
 
-            ftp.retrbinary('RETR {}'.format(gtfs), cb)
+            ftp.retrbinary('RETR {}'.format(GTFS_NAME), cb)
 
 def main():
 
-    if not os.path.isdir(savedir):
+    if not os.path.isdir(SAVE_DIR):
         print('Temp directory is not present. Creating a new one.')
-        os.mkdir(savedir)
+        os.mkdir(SAVE_DIR)
     else:
         print('Temp directory is present.')
 
-    os.chdir(savedir)
+    os.chdir(SAVE_DIR)
 
-    remote_datetime = ftp.voidcmd("MDTM " + gtfs)[4:].strip()
+    remote_datetime = ftp.voidcmd("MDTM " + GTFS_NAME)[4:].strip()
     remote_timestamp = time.mktime(time.strptime(remote_datetime, '%Y%m%d%H%M%S'))
 
-    if os.path.isfile(savedir + "/" + gtfs):
+    if os.path.isfile(SAVE_DIR + "/" + GTFS_NAME):
         print("Found local gtfs archive.")
 
     else:
         print("Local gtfs archive not found, Downloading.")
         gtfs_download()
 
-    local_gtfs_timestamp = os.path.getmtime(gtfs)
+    local_gtfs_timestamp = os.path.getmtime(GTFS_NAME)
 
     if int(remote_timestamp) > int(local_gtfs_timestamp):
         print("Local file has expired, Retriving up to date file")
@@ -57,18 +55,18 @@ def main():
 
     FTP.quit(ftp)
 
-    if not os.path.isdir(gtfs_dir):
+    if not os.path.isdir(GTFS_DIR):
         print('Working directory is not present. Creating a new one.')
-        os.mkdir(gtfs_dir)
+        os.mkdir(GTFS_DIR)
     else:
         print('Working directory is present.')
 
-    with zipfile.ZipFile(gtfs) as zip:
+    with zipfile.ZipFile(GTFS_DIR) as zip:
         print("Extracting")
         for item in tqdm(zip.infolist(), total=len(zip.namelist()), unit='Files'):
             zip.filename = os.path.basename(zip.filename)
             try:
-                zip.extract(item, gtfs_dir)
+                zip.extract(item, GTFS_DIR)
             except zipfile.error() as error:
                 pass
 
