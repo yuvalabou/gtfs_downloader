29ed0c97cf84542ff335fada341b3f2a4e9ba908
yuval aboulafia <yuval.abou@gmail.com>
2020-09-16 09:27:35 +0300

    Class update

--- README.md
+++ README.md
@@ -1,10 +1,37 @@
-Python app to retrive and extract gtfs data for israely public transport
+# Python program to retrive and extract gtfs data for Israeli public transport
 
-#### Requierments
+## features
+
+Downloads the up-to-date gtfs archive and extract it.
+the program compares the local copy against the remote file for an up to date data.
+
+### Requierments
 
 - tqdm
 
-### features:
-app compares the local against the remote file for up to date data
+## How to use
 
-### How to use:
+Fork the repository:
+
+Clone the repository to your local machine and navigate to the downloaded folder.
+
+```bash
+git clone https://github.com/YOURUSERNAME/gtfs_downloader
+cd gtfs_downloader
+```
+
+Run the app:
+
+```python
+python3 gtfs_downloader.py
+```
+
+------
+
+### Please support my motivation to learn and share
+
+<a href="https://www.buymeacoffee.com/HMa8m26"
+    target="_blank">
+        <img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png"
+            alt="Buy Me A Coffee" style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" >
+</a>
--- gtfs_downloader.py
+++ gtfs_downloader.py
@@ -1,30 +1,49 @@
+"""Get GTFS data from israeli public transport and extract to a working dir."""
+
 from ftplib import FTP
-import zipfile
 import os
 import time
 from tqdm import tqdm
+import zipfile
 
 SAVE_DIR = "./Temp/"
-GTFS_DIR = "../gtfs/"
+GTFS_DIR = "../gtfs"
 GTFS_NAME = "israel-public-transportation.zip"
 
 ftp = FTP('199.203.58.18')
 ftp.login()
 print("Connected")
 
-def gtfs_download():
-    with open(GTFS_NAME, 'wb') as fd:
-        total = int(ftp.size(GTFS_NAME))
+class gtfs_downloader():
+    """Downloader class."""
 
-        with tqdm(total=total, unit='B', unit_scale=True) as pbar:
-            def cb(data):
-                l = int(len(data))
-                pbar.update(l)
-                fd.write(data)
+    def download(self=None):
+        """Download the archive."""
+        with open(GTFS_NAME, 'wb') as fd:
+            total = int(ftp.size(GTFS_NAME))
+            with tqdm(total=total, unit='B', unit_scale=True) as pbar:
+                def cb(data):
+                    l = int(len(data))
+                    pbar.update(l)
+                    fd.write(data)
+                ftp.retrbinary('RETR {}'.format(GTFS_NAME), cb)
 
-            ftp.retrbinary('RETR {}'.format(GTFS_NAME), cb)
+    def extract(self=None):
+        """Extract the downloaded file."""
+        with zipfile.ZipFile(GTFS_NAME) as zip:
+            print("Extracting")
+            for i in tqdm(zip.infolist(), total=len(zip.namelist()), unit='Files'):
+                zip.filename = os.path.basename(zip.filename)
+                try:
+                    zip.extract(i, GTFS_DIR)
+                except zipfile.error() as error:
+                    print("Unexpected error:", error)
+                    raise
+        zip.close()
+
 
 def main():
+    """Main Program."""
 
     if not os.path.isdir(SAVE_DIR):
         print('Temp directory is not present. Creating a new one.')
@@ -37,21 +56,21 @@ def main():
     remote_datetime = ftp.voidcmd("MDTM " + GTFS_NAME)[4:].strip()
     remote_timestamp = time.mktime(time.strptime(remote_datetime, '%Y%m%d%H%M%S'))
 
-    if os.path.isfile(SAVE_DIR + "/" + GTFS_NAME):
+    if os.path.isfile(f"{0}{1}".format(SAVE_DIR, GTFS_NAME)):
         print("Found local gtfs archive.")
-
+        pass
     else:
         print("Local gtfs archive not found, Downloading.")
-        gtfs_download()
+        gtfs_downloader.download()
 
     local_gtfs_timestamp = os.path.getmtime(GTFS_NAME)
 
-    if int(remote_timestamp) > int(local_gtfs_timestamp):
+    if int(remote_timestamp) != int(local_gtfs_timestamp):  # FIXME: Need to check for days, not just timestamp
         print("Local file has expired, Retriving up to date file")
-        gtfs_download()
-
+        gtfs_downloader.download()
     else:
         print("Local file is up to date.")
+        pass
 
     FTP.quit(ftp)
 
@@ -60,18 +79,11 @@ def main():
         os.mkdir(GTFS_DIR)
     else:
         print('Working directory is present.')
+        pass
 
-    with zipfile.ZipFile(GTFS_DIR) as zip:
-        print("Extracting")
-        for item in tqdm(zip.infolist(), total=len(zip.namelist()), unit='Files'):
-            zip.filename = os.path.basename(zip.filename)
-            try:
-                zip.extract(item, GTFS_DIR)
-            except zipfile.error() as error:
-                pass
-
-    zip.close()
+    gtfs_downloader.extract()
     print("Extracted to ../gtfs/ folder.")
+
 
 if __name__ == "__main__":
     main()
